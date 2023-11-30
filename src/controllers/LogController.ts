de8f0168d883b62f5999601ea41ec9e4ecb886ed
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2023-11-30 08:22:33 -0300

    updated

--- src/controllers/LogController.ts
+++ src/controllers/LogController.ts
@@ -4,13 +4,25 @@ class LogController {
 
     public async writeToTxt(textData: string): Promise<void> {
         try {
-            const nomeDoArquivo =  'logs.txt'; 
+            const nomeDoArquivo = 'logs.txt';
             const conteudoParaEscrever = `Requisição: ${new Date()} - ${textData}\n`;
             await fs.promises.appendFile(nomeDoArquivo, conteudoParaEscrever);
             console.log('Conteúdo escrito com sucesso!');
         } catch (error) {
             console.error('Erro inesperado:', error);
             throw new Error('Erro inesperado');
+        }
+    }
+    public async searchByDateTime(dateTime: string): Promise<string | null> {
+        try {
+            const nomeDoArquivo = 'logs.txt';
+            const conteudoDoArquivo = await fs.promises.readFile(nomeDoArquivo, 'utf-8');
+            const linhas = conteudoDoArquivo.split('\n');
+            const linhaEncontrada = linhas.find((linha) => linha.includes(dateTime));
+            return linhaEncontrada || null;
+        } catch (error) {
+            console.error('Erro inesperado:', error);
+            throw new Error('Erro inesperado na busca');
         }
     }
 }
--- src/routes/LogRouter.ts
+++ src/routes/LogRouter.ts
@@ -3,6 +3,23 @@ import { Router } from "express";
 
 const routes = Router();
 
+routes.get('/busca', async (req, res) => {
+    try {
+        const { dateTime } = req.query;
+        if (!dateTime) {
+            return res.status(400).json({ error: 'Parâmetro dateTime é obrigatório.' });
+        }
 
+        const resultadoBusca = await LogController.searchByDateTime(dateTime as string);
+        if (resultadoBusca) {
+            res.status(200).json({ resultado: resultadoBusca });
+        } else {
+            res.status(404).json({ message: 'Nenhum registro encontrado para a data e hora especificadas.' });
+        }
+    } catch (error) {
+        console.error('Erro durante a busca de logs:', error);
+        res.status(500).json({ error: 'Erro durante a busca de logs.' });
+    }
+});
 
 export default routes;
