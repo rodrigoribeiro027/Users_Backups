de5fd74919ca4ef160e71dd6f3436d8420a41127
Nicolas <nichollaslimma734@gmail.com>
2023-12-03 20:12:28 -0300

    atualizado login, para verificar versão do termo aceito pelo usuario

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -2,15 +2,31 @@ import { Request, Response } from 'express';
 import { Usuarios } from '../models/Usuarios';
 import { generateToken } from '../middleware/authenticate';
 import LogController from './LogController';
+import TermoService from '../services/TermoService';
 
 
 class LoginController{
     public async login(req: Request, res: Response){
         try{
             const { email, senha } = req.body;
-            const usuario = await Usuarios.findOne({email: email, senha:senha}, "-__v");
+            const usuario = await Usuarios.findOne({email: email, senha:senha}, "-__v").populate({
+                path: 'TermosUso',
+                populate: {
+                    path: 'termo'
+                }
+            });
             if(!usuario){
                 return res.status(404).json(`usuario não encontrado, email ou senha incorreto....`);
+            }
+            let termo;
+            if(usuario.TermosUso.length > 1){
+                termo = usuario.TermosUso.reduce((a, b) => a.dataRegistro > b.dataRegistro ? a : b);
+            }else{
+                termo = usuario.TermosUso[0];
+            }
+            const lastTermo = await TermoService.findLastVersion();
+            if(termo.termo.versao < lastTermo.versao){
+                return res.status(403).json({message:'Os termos de uso foram atualizados, por favor verifique e aceite novamente.'});
             }
             const token = await generateToken(usuario);
             const logText = `Login realizado token: ${token}`;
